93b614422aa155fd6747ba5a9ad360135032fdce
东水水 <13855141090@163.com>
2023-02-14 16:28:17 +0800

    service-map项目创建

---
+++ service-map/src/main/java/com/dongshuishui/servicemap/ServiceMapApplication.java
@@ -0,0 +1,17 @@
+package com.dongshuishui.servicemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: 东水水
+ * @Date: 2023/2/14  16:22
+ * @Description: com.dongshuishui.servicemap
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class ServiceMapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceMapApplication.class);
+    }
+}
