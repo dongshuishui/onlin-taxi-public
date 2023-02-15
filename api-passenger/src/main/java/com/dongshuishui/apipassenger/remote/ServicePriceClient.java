080fe82df3bf90b8a5fc59b23938c288245f55dc
东水水 <13855141090@163.com>
2023-02-15 16:08:49 +0800

    预估价格编写完成

---
+++ api-passenger/src/main/java/com/dongshuishui/apipassenger/remote/ServicePriceClient.java
@@ -0,0 +1,22 @@
+package com.dongshuishui.apipassenger.remote;
+
+import com.dongshuishui.internalcommon.dto.ResponseResult;
+import com.dongshuishui.request.ForecastPriceDTO;
+import com.dongshuishui.response.ForecastPriceResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @Author: 东水水
+ * @Date: 2023/2/15  15:56
+ * @Description: com.dongshuishui.apipassenger.remote
+ * @Version: 1.0
+ */
+@FeignClient("service-price")
+public interface ServicePriceClient {
+
+    @RequestMapping(method = RequestMethod.POST,value = "/forecast-price")
+    public ResponseResult<ForecastPriceResponse> forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO);
+}
--- api-passenger/src/main/java/com/dongshuishui/apipassenger/service/ForecastPriceService.java
+++ api-passenger/src/main/java/com/dongshuishui/apipassenger/service/ForecastPriceService.java
@@ -1,8 +1,11 @@
 package com.dongshuishui.apipassenger.service;
 
+import com.dongshuishui.apipassenger.remote.ServicePriceClient;
 import com.dongshuishui.internalcommon.dto.ResponseResult;
+import com.dongshuishui.request.ForecastPriceDTO;
 import com.dongshuishui.response.ForecastPriceResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,6 +17,9 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class ForecastPriceService {
+
+    @Autowired
+    private ServicePriceClient servicePriceClient;
 
     /**
      * 根据 出发地经纬度 和 目的地经纬度 来获取预估价格
@@ -28,10 +34,16 @@ public class ForecastPriceService {
         log.info("出发地经度：" + depLongitude +",出发地纬度：" + depLatitude );
         log.info("目的地经度：" + destLongitude + ",目的地纬度：" + destLatitude);
         log.info("调用计价服务，计算价格");
-
+        ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+        forecastPriceDTO.setDepLongitude(depLongitude);
+        forecastPriceDTO.setDepLatitude(depLatitude);
+        forecastPriceDTO.setDestLongitude(destLongitude);
+        forecastPriceDTO.setDestLatitude(destLatitude);
+        ResponseResult<ForecastPriceResponse> responseResult = servicePriceClient.forecastPrice(forecastPriceDTO);
+        double price = responseResult.getData().getPrice();
 
         ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
-        forecastPriceResponse.setPrice(12.34);
+        forecastPriceResponse.setPrice(price);
         return ResponseResult.success(forecastPriceResponse);
     }
 }
