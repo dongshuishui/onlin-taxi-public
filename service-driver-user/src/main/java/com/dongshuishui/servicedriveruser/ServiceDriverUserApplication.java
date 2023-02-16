bbed87925479942ad3a10d5b076c084eab521421
东水水 <13855141090@163.com>
2023-02-16 19:35:25 +0800

    server-driver-user注册到nacos中

--- service-driver-user/src/main/java/com/dongshuishui/servicedriveruser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/dongshuishui/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,6 +3,7 @@ package com.dongshuishui.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: 东水水
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Version: 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.dongshuishui.servicedriveruser.mapper")
 public class ServiceDriverUserApplication {
     public static void main(String[] args) {
